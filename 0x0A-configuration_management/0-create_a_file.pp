1aaf3fa6a931f29daa6722252e58cc860e3b31ae
TheSeekerSeeker <odondiezra5@gmail.com>
2022-09-17 22:31:45 +0300

    updated

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# creates a file in the /tmp directory
+#creating a file
+
 file { '/tmp/school':
   ensure  => 'file',
+  content => 'I love Puppet',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
 }
